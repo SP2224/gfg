49a07dd71075a38e8b4c7180143b5cee54daedc7
Satya Prakash Das <sprakash19254@gmail.com>
2022-06-26 10:02:35 +0530

    first commit

---
+++ src/reader/gfg1.java
@@ -0,0 +1,32 @@
+package reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class gfg1 {
+	public static void main(String[] args) throws IOException
+	{
+		FileReader fr = new FileReader("E:\\eclipse workspace\\gfg java\\src\\reader\\file2.txt");
+		BufferedReader br = new BufferedReader(fr);
+		char c[] = new char[20];
+		if(br.markSupported()) {
+			System.out.println("mark() method is supported");
+			br.mark(100);
+		}
+		br.skip(8);
+		if(br.ready()) {
+			System.out.println(br.readLine());
+			br.read(c);
+			for(int i=0;i<20;i++)
+			{
+				System.out.println(c[i]);
+			}
+			System.out.println();
+			br.reset();
+			for(int i=0;i<8;i++) {
+				System.out.println((char)br.read());
+			}
+		}
+	}
+}
